738616260780d6fbb3d3eb1e5512b85bfb103989
M. Sohaib Alam <sohaib.alam@gmail.com>
2019-06-21 12:03:52 -0700

    Calculating PauliZ expectation using measure_observables

--- pennylane_forest/qpu.py
+++ pennylane_forest/qpu.py
@@ -27,6 +27,16 @@ from pyquil import get_qc
 from .device import ForestDevice
 from ._version import __version__
 
+import numpy as np
+
+from pyquil import get_qc
+from pyquil.api._quantum_computer import _get_qvm_with_topology
+from pyquil.gates import MEASURE, RESET
+from pyquil.quil import Pragma, Program
+from pyquil.paulis import sX, sY, sZ
+from pyquil.operator_estimation import ExperimentSetting, TensorProductState, TomographyExperiment, measure_observables, group_experiments
+from pyquil.quilbase import Gate
+
 
 class QPUDevice(ForestDevice):
     r"""Forest QPU device for PennyLane.
@@ -80,7 +90,114 @@ class QPUDevice(ForestDevice):
         self.active_reset = active_reset
 
     def pre_expval(self):
-        pass
+        # pass
+        ## code below borrowed from `qvm.py`
+        """Run the QVM"""
+        # pylint: disable=attribute-defined-outside-init
+        for e in self.expval_queue:
+            wires = e.wires
 
-    def expval(self):
-        pass
+            if e.name == 'PauliX':
+                # X = H.Z.H
+                self.apply('Hadamard', wires, [])
+
+            elif e.name == 'PauliY':
+                # Y = (HS^)^.Z.(HS^) and S^=SZ
+                self.apply('PauliZ', wires, [])
+                self.apply('S', wires, [])
+                self.apply('Hadamard', wires, [])
+
+            elif e.name == 'Hadamard':
+                # H = Ry(-pi/4)^.Z.Ry(-pi/4)
+                self.apply('RY', wires, [-np.pi/4])
+
+            elif e.name == 'Hermitian':
+                # For arbitrary Hermitian matrix H, let U be the unitary matrix
+                # that diagonalises it, and w_i be the eigenvalues.
+                H = e.parameters[0]
+                Hkey = tuple(H.flatten().tolist())
+
+                if Hkey in self._eigs:
+                    # retrieve eigenvectors
+                    U = self._eigs[Hkey]['eigvec']
+                else:
+                    # store the eigenvalues corresponding to H
+                    # in a dictionary, so that they do not need to
+                    # be calculated later
+                    w, U = np.linalg.eigh(H)
+                    self._eigs[Hkey] = {'eigval': w, 'eigvec': U}
+
+                # Perform a change of basis before measuring by applying U^ to the circuit
+                self.apply('QubitUnitary', wires, [U.conj().T])
+
+        prag = Program(Pragma('INITIAL_REWIRING', ['"PARTIAL"']))
+
+        if self.active_reset:
+            prag += RESET()
+
+        self.prog = prag + self.prog
+
+        qubits = list(self.prog.get_qubits())
+        ro = self.prog.declare('ro', 'BIT', len(qubits))
+        for i, q in enumerate(qubits):
+            self.prog.inst(MEASURE(q, ro[i]))
+
+        self.prog.wrap_in_numshots_loop(self.shots)
+
+        if "pyqvm" in self.qc.name:
+            bitstring_array = self.qc.run(self.prog)
+        else:
+            executable = self.qc.compile(self.prog)
+            bitstring_array = self.qc.run(executable=executable)
+
+        self.state = {}
+        for i, q in enumerate(qubits):
+            self.state[q] = bitstring_array[:, i]
+
+    def expval(self, expectation, wires, par):
+        d_expectation = {'PauliZ': sZ}
+        if len(wires) == 1:
+            qubit = self.qc.qubits()[0]
+            prep_prog = Program([instr for instr in self.program if isinstance(instr, Gate)])
+            expt_setting = ExperimentSetting(TensorProductState(), d_expectation[expectation](qubit))
+            tomo_expt = TomographyExperiment(settings=[expt_setting], program=prep_prog)
+            grouped_tomo_expt = group_experiments(tomo_expt)
+            meas_obs = list(measure_observables(self.qc, grouped_tomo_expt))
+            return np.sum(expt_result.expectation for expt_result in meas_obs)
+
+        # if len(wires) == 1:
+        #     # 1 qubit observable
+        #     evZ = np.mean(1-2*self.state[wires[0]])
+
+        #     # for single qubit state probabilities |psi|^2 = (p0, p1),
+        #     # we know that p0+p1=1 and that <Z>=p0-p1
+        #     p0 = (1+evZ)/2
+        #     p1 = (1-evZ)/2
+
+        #     if expectation == 'Identity':
+        #         # <I> = \sum_i p_i
+        #         return p0 + p1
+
+        #     if expectation == 'Hermitian':
+        #         # <H> = \sum_i w_i p_i
+        #         Hkey = tuple(par[0].flatten().tolist())
+        #         w = self._eigs[Hkey]['eigval']
+        #         return w[0]*p0 + w[1]*p1
+
+        #     return evZ
+
+        # # Multi-qubit observable
+        # # ----------------------
+        # # Currently, we only support qml.expval.Hermitian(A, wires),
+        # # where A is a 2^N x 2^N matrix acting on N wires.
+        # #
+        # # Eventually, we will also support tensor products of Pauli
+        # # matrices in the PennyLane UI.
+
+        # probs = self.probabilities(wires)
+
+        # if expectation == 'Hermitian':
+        #     Hkey = tuple(par[0].flatten().tolist())
+        #     w = self._eigs[Hkey]['eigval']
+        #     # <A> = \sum_i w_i p_i
+        #     return w @ probs
