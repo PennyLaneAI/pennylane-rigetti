5faa56e3331e4c7491000243ff6b96210039fa97
Josh Izaac <josh146@gmail.com>
2019-07-29 22:44:19 +0800

    added tests

--- tests/test_qvm.py
+++ tests/test_qvm.py
@@ -165,7 +165,7 @@ class TestQVMBasic(BaseTest):
         ev2 = ((a - d) * np.cos(theta) * np.cos(phi) + 2 * re_b * np.sin(phi) + a + d) / 2
         expected = np.array([ev1, ev2])
 
-        self.assertAllAlmostEqual(res, expected, delta=3 / np.sqrt(shots))
+        self.assertAllAlmostEqual(res, expected, delta=4 / np.sqrt(shots))
 
     def test_multi_mode_hermitian_expectation(self, shots, qvm, compiler):
         """Test that arbitrary multi-mode Hermitian expectation values are correct"""
@@ -254,7 +254,7 @@ class TestQVMBasic(BaseTest):
             + 39
         )
 
-        self.assertAlmostEqual(var, expected, delta=0.2)
+        self.assertAlmostEqual(var, expected, delta=0.3)
 
     @pytest.mark.parametrize(
         "gate", plf.QVMDevice._operation_map
@@ -308,6 +308,111 @@ class TestQVMBasic(BaseTest):
         # Note we have increased the tolerance here, since we are only
         # performing 1024 shots.
         self.assertAllAlmostEqual(res, expected, delta=3 / np.sqrt(shots))
+
+    def test_sample_values(self, tol):
+        """Tests if the samples returned by sample have
+        the correct values
+        """
+        dev = plf.QVMDevice(device="1q-qvm", shots=10)
+
+        dev.apply('RX', wires=[0], par=[1.5708])
+        dev._obs_queue = [qml.PauliZ(wires=[0], do_queue=False)]
+        dev.pre_measure()
+
+        s1 = dev.sample('PauliZ', [0], [], 10)
+
+        # s1 should only contain 1 and -1
+        self.assertAllAlmostEqual(s1**2, 1, delta=tol)
+        self.assertAllAlmostEqual(s1, 1-2*dev.state[0], delta=tol)
+
+    def test_sample_values_hermitian(self, tol):
+        """Tests if the samples of a Hermitian observable returned by sample have
+        the correct values
+        """
+        theta = 0.543
+        shots = 100000
+        A = np.array([[1, 2j], [-2j, 0]])
+
+        dev = plf.QVMDevice(device="1q-qvm", shots=shots)
+
+        dev.apply('RX', wires=[0], par=[theta])
+        dev._obs_queue = [qml.Hermitian(A, wires=[0], do_queue=False)]
+        dev.pre_measure()
+
+        s1 = dev.sample('Hermitian', [0], [A], shots)
+
+        # s1 should only contain the eigenvalues of
+        # the hermitian matrix
+        eigvals = np.linalg.eigvalsh(A)
+        assert np.allclose(sorted(list(set(s1))), sorted(eigvals), atol=tol, rtol=0)
+
+        # the analytic mean is 2*sin(theta)+0.5*cos(theta)+0.5
+        assert np.allclose(np.mean(s1), 2*np.sin(theta)+0.5*np.cos(theta)+0.5, atol=0.1, rtol=0)
+
+        # the analytic variance is 0.25*(sin(theta)-4*cos(theta))^2
+        assert np.allclose(np.var(s1), 0.25*(np.sin(theta)-4*np.cos(theta))**2, atol=0.1, rtol=0)
+
+    def test_sample_values_hermitian_multi_qubit(self, tol):
+        """Tests if the samples of a multi-qubit Hermitian observable returned by sample have
+        the correct values
+        """
+        theta = 0.543
+        shots = 100000
+
+        A = np.array([
+            [1,     2j,   1-2j, 0.5j  ],
+            [-2j,   0,    3+4j, 1     ],
+            [1+2j,  3-4j, 0.75, 1.5-2j],
+            [-0.5j, 1,    1.5+2j, -1  ]
+        ])
+
+        dev = plf.QVMDevice(device="2q-qvm", shots=shots)
+
+        dev.apply('RX', wires=[0], par=[theta])
+        dev.apply('RY', wires=[1], par=[2*theta])
+        dev.apply('CNOT', wires=[0, 1], par=[])
+        dev._obs_queue = [qml.Hermitian(A, wires=[0, 1], do_queue=False)]
+        dev.pre_measure()
+
+        s1 = dev.sample('Hermitian', [0, 1], [A], shots)
+
+        # s1 should only contain the eigenvalues of
+        # the hermitian matrix
+        eigvals = np.linalg.eigvalsh(A)
+        assert np.allclose(sorted(list(set(s1))), sorted(eigvals), atol=tol, rtol=0)
+
+        # make sure the mean matches the analytic mean
+        expected = (88*np.sin(theta) + 24*np.sin(2*theta) - 40*np.sin(3*theta)
+            + 5*np.cos(theta) - 6*np.cos(2*theta) + 27*np.cos(3*theta) + 6)/32
+        assert np.allclose(np.mean(s1), expected, atol=0.1, rtol=0)
+
+    def test_sample_exception_analytic_mode(self):
+        """Tests if the sampling raises an error for sample size n=0
+        """
+        dev = plf.QVMDevice(device="1q-qvm", shots=10)
+        dev.apply('RX', wires=[0], par=[0.4])
+        dev._obs_queue = [qml.PauliZ(wires=0, do_queue=False)]
+        dev.pre_measure()
+
+        with pytest.raises(ValueError, match="Calling sample with n = 0 is not possible"):
+            dev.sample('PauliZ', [0], [], n=0)
+
+    @pytest.mark.parametrize("n", [-12, 12.3])
+    def test_sample_exception_wrong_n(self, n):
+        """Tests if the sampling raises an error for sample size n<0
+        or non-integer n
+        """
+        dev = plf.QVMDevice(device="1q-qvm", shots=10)
+        dev.apply('RX', wires=[0], par=[0.4])
+        dev._obs_queue = [qml.PauliZ(wires=0, do_queue=False)]
+        dev.pre_measure()
+
+        with pytest.raises(ValueError, match="The number of samples must be a positive integer"):
+            dev.sample('PauliZ', [0], [], n=n)
+
+        # self.def.shots = 0, so this should also fail
+        with pytest.raises(ValueError, match="The number of samples must be a positive integer"):
+            dev.sample('PauliZ', [0], [], n=n)
 
 
 class TestQVMIntegration(BaseTest):
