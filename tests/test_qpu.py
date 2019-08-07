af2f269baf0872e4452a613db1eae23378fe5af1
M. Sohaib Alam <sohaib.alam@gmail.com>
2019-08-07 13:19:36 -0700

    Initializing a device once within the class

--- tests/test_qpu.py
+++ tests/test_qpu.py
@@ -18,28 +18,29 @@ VALID_QPU_LATTICES = [qc for qc in pyquil.list_quantum_computers() if "qvm" not 
 class TestQPUIntegration(BaseTest):
     """Test the wavefunction simulator works correctly from the PennyLane frontend."""
 
+    def __init__(self):
+        self.device = [qpu for qpu in VALID_QPU_LATTICES if '2Q' in qpu][0]
+
     # pylint: disable=no-self-use
 
     def test_load_qpu_device(self):
-        device = [qpu for qpu in VALID_QPU_LATTICES if '2Q' in qpu][0]
         """Test that the QPU device loads correctly"""
-        dev = qml.device("forest.qpu", device=device, load_qc=False)
+        dev = qml.device("forest.qpu", device=self.device, load_qc=False)
         self.assertEqual(dev.num_wires, 2)
         self.assertEqual(dev.shots, 1024)
         self.assertEqual(dev.short_name, "forest.qpu")
 
     def test_load_virtual_qpu_device(self):
         """Test that the QPU simulators load correctly"""
-        qml.device("forest.qpu", device="Aspen-4-2Q-A", load_qc=False)
+        qml.device("forest.qpu", device=self.device, load_qc=False)
 
     def test_qpu_args(self):
         """Test that the QPU plugin requires correct arguments"""
-        device = VALID_QPU_LATTICES[0]
         with pytest.raises(ValueError, match="QPU device does not support a wires parameter"):
-            qml.device("forest.qpu", device=device, wires=2)
+            qml.device("forest.qpu", device=self.device, wires=2)
 
         with pytest.raises(TypeError, match="missing 1 required positional argument"):
             qml.device("forest.qpu")
 
         with pytest.raises(ValueError, match="Number of shots must be a positive integer"):
-            qml.device("forest.qpu", device=device, shots=0)
+            qml.device("forest.qpu", device=self.device, shots=0)
