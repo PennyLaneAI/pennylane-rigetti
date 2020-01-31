c2df2e8c2c36f2d7e3555462b85b5e63819192ca
antalszava <antalszava@gmail.com>
2020-01-31 01:56:11 -0500

    Modify timeout for test where server failed to respond

--- tests/test_qvm.py
+++ tests/test_qvm.py
@@ -962,7 +962,7 @@ class TestQVMIntegration(BaseTest):
     @pytest.mark.parametrize("device", ["2q-qvm", np.random.choice(VALID_QPU_LATTICES)])
     def test_compiled_program_was_stored_mutable_qnode_with_loop(self, qvm, device):
         """Test that QVM device stores the compiled program when the QNode is mutated correctly"""
-        dev = qml.device("forest.qvm", device=device, timeout=100)
+        dev = qml.device("forest.qvm", device=device, timeout=120)
 
         assert len(dev._compiled_program_dict.items()) == 0
 
