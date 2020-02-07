c5b2ab9f3681c0391822b2486ead50289c5b18c1
antalszava <antalszava@gmail.com>
2020-02-07 07:10:18 -0500

    Test on wires expval with operator estimation

--- tests/test_qpu.py
+++ tests/test_qpu.py
@@ -57,17 +57,49 @@ class TestQPUIntegration(BaseTest):
         with pytest.raises(ValueError, match="Readout error cannot be set on the physical QPU"):
             qml.device("forest.qpu", device=device, load_qc=True, readout_error=[0.9, 0.75])
 
-    @flaky(max_runs=5, min_passes=3)
+    @flaky(max_runs=3, min_passes=2)
     @pytest.mark.parametrize("obs", [qml.PauliX(0), qml.PauliZ(0), qml.PauliY(0), qml.Hadamard(0), qml.Identity(0)])
-    def test_tensor_wires_expval(self, obs):
+    def test_tensor_wires_expval_parametric_compilation(self, obs):
         """Test the QPU expval method for Tensor observables made up of a single observable when parametric compilation is
-        turned off.
+        turned on.
 
-        As the results coming from the qvm are stochastic, a constraint of 3 out of 5 runs was added.
+        As the results coming from the qvm are stochastic, a constraint of 2 out of 3 runs was added.
         """
         p = np.pi/7
         dev = qml.device('forest.qpu', device='Aspen-4-4Q-E', shots=10000, load_qc=False, parametric_compilation=True)
         dev_1 = qml.device('forest.qpu', device='Aspen-4-4Q-E', shots=10000, load_qc=False,  parametric_compilation=True)
+
+        def template(param):
+            qml.BasisState(np.array([0, 0, 1, 1]), wires=list(range(4)))
+            qml.RY(param, wires=[2])
+            qml.CNOT(wires=[2, 3])
+
+        @qml.qnode(dev)
+        def circuit_tensor(param):
+            template(param)
+            return qml.expval(Tensor(obs))
+
+        @qml.qnode(dev_1)
+        def circuit_obs(param):
+            template(param)
+            return qml.expval(obs)
+
+        res = circuit_tensor(p)
+        exp = circuit_obs(p)
+
+        assert np.allclose(res, exp, atol=2e-2)
+
+    @flaky(max_runs=5, min_passes=3)
+    @pytest.mark.parametrize("obs", [qml.PauliX(0), qml.PauliZ(0), qml.PauliY(0), qml.Hadamard(0), qml.Identity(0)])
+    def test_tensor_wires_expval_operator_estimation(self, obs):
+        """Test the QPU expval method for Tensor observables made up of a single observable when parametric compilation is
+        turned off allowing operator estimation.
+
+        As the results coming from the qvm are stochastic, a constraint of 3 out of 5 runs was added.
+        """
+        p = np.pi/7
+        dev = qml.device('forest.qpu', device='Aspen-4-4Q-E', shots=1000, load_qc=False, parametric_compilation=False)
+        dev_1 = qml.device('forest.qpu', device='Aspen-4-4Q-E', shots=1000, load_qc=False,  parametric_compilation=False)
 
         def template(param):
             qml.BasisState(np.array([0, 0, 1, 1]), wires=list(range(4)))
