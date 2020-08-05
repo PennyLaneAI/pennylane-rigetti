fad9e37232ae6deb913e8b5c665b2a8c80397609
Josh Izaac <josh146@gmail.com>
2020-08-05 18:19:18 +0930

    fix import

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -6,7 +6,7 @@ import pennylane as qml
 import pyquil
 import pyquil.gates as g
 import pytest
-from pennylane.utils import OperationRecorder
+from pennylane._queuing import OperationRecorder
 from pennylane_forest.converter import *
 
 
