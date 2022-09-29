e0d9fb532d5d503a1aeeec8ec77b6b9a8a31018a
marquessv <marquessavaldez@gmail.com>
2022-09-29 13:39:15 -0700

    update pyquil to v3, add qcs-api-client

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("pennylane_forest/_version.py") as f:
     version = f.readlines()[-1].split()[-1].strip("\"'")
 
 
-requirements = ["pyquil>=2.16,<2.28.3", "pennylane>=0.17"]
+requirements = ["pyquil>=3.3.1,<4.0.0", "qcs-api-client>=0.20.13<0.22.0", "pennylane>=0.17"]
 
 info = {
     "name": "PennyLane-Forest",
